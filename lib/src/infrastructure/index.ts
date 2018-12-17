c90efecf31f2cc79808508cb841d44e7dcb5c711
Szymon Piecuch <szymek.piecuch@gmail.com>
2018-12-17 21:18:23 +0100

    Fixed remote procedure export

--- lib/src/infrastructure/index.ts
+++ lib/src/infrastructure/index.ts
@@ -2,4 +2,5 @@ export * from './database/mongo.database';
 export * from './id/objectid.factory';
 export * from './pagination/mongodb.paginator';
 export * from './logger/null.logger';
+export * from './remote-procedure/null.remote-procedure';
 export * from './container/awilix.decorator';
